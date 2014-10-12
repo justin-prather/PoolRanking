a8b12d827a398e1f4297cd961109f2320810226c
Justin Prather <justinprather@mac.com>
2014-10-11 17:39:54 -0700

    done sliders

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
   var $table = $('#table-body');
   var table_template = $('#table-entry-template').html();
   var $spinner = $('.spinner');
@@ -6,12 +6,35 @@ $(function(){
   $.ajax({
     type: 'GET',
     url: '/player',
-    success: function(players){
-      $.each( players, function(i, player){
-        $table.append(Mustache.render(table_template, player ));
+    success: function(players) {
+      $.each(players, function(i, player) {
+        $table.append(Mustache.render(table_template, player));
       });
 
       $spinner.addClass(".hidden");
+    }
+  });
+});
+
+$(function() {
+  $(".slider").click(function(event) {
+    if (event.target.id == "player-head") {
+      if (!$("#player-form").hasClass("show")) {
+        $('#player-form').slideDown();
+        $('#player-form').addClass("show");
+      } else {
+        $('#player-form').slideUp();
+        $('#player-form').removeClass("show");
+      }
+    }
+    else if (event.target.id == "match-head") {
+      if (!$("#match-form").hasClass("show")) {
+        $('#match-form').slideDown();
+        $('#match-form').addClass("show");
+      } else {
+        $('#match-form').slideUp();
+        $('#match-form').removeClass("show");
+      }
     }
   });
 });
